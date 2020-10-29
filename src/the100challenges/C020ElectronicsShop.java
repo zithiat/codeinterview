549a3b5ea09dd5dc775aa6162faf0bee3e2f0ed4
Zithiat <quan.doanh@gmail.com>
2020-10-29 11:15:10 -0400

    Update C020ElectronicsShop.java

--- src/the100challenges/C020ElectronicsShop.java
+++ src/the100challenges/C020ElectronicsShop.java
@@ -1,5 +1,8 @@
 package the100challenges;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class C020ElectronicsShop {
 
 	/**
@@ -15,10 +18,25 @@ public class C020ElectronicsShop {
 	 * 				- m: the number of USB drive models.
 	 * 				The second line contains n space-separated integers keyboard[i], the prices of each keyboard model.
 	 * 				The third line contains m space-separated integers drives, the prices of the USB drives.
-	 * Output:		Print a single integer denoting the amount of money Monica will spend. If she doesn’t have enough money to buy one keyboard and one USB drive, print -1instead.
+	 * Output:		Print a single integer denoting the amount of money Monica will spend. 
+	 * 				If she doesn’t have enough money to buy one keyboard and one USB drive, print -1instead.
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Random r = new Random();
+		int b = r.nextInt(1000);
+		int n = 10;
+		int m = 15;
+		int rn = 100;
+		int[] kArr = new int[n];
+		int[] uArr = new int[m];
+		IntStream.range(0, n).forEach(i -> kArr[i] = r.nextInt(rn));
+		IntStream.range(0, m).forEach(i -> uArr[i] = r.nextInt(rn));
+		findSpentAmount(b, kArr, n, uArr, m);
+	}
 
+	private static void findSpentAmount(int b, int[] kArr, int n, int[] uArr, int m) {
+		// TODO Auto-generated method stub
+		
 	}
 }
